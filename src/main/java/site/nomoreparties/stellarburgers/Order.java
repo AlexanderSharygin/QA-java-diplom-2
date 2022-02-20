2c439b86c44523f965097b2444a721324cfa7643
= <AlexSharygin1989@yandex.ru>
2022-02-20 10:01:15 +0000

    CreateOrdersTestsImplemened

---
+++ src/main/java/site/nomoreparties/stellarburgers/Order.java
@@ -0,0 +1,28 @@
+package site.nomoreparties.stellarburgers;
+
+import com.google.gson.annotations.SerializedName;
+import org.apache.commons.lang3.RandomStringUtils;
+
+import java.util.List;
+
+public class Order {
+
+    @SerializedName("ingredients")
+    String[] ingredientsId;
+
+    public Order(List<Ingredient> ingredientsId) {
+        if (ingredientsId == null) {
+            this.ingredientsId = null;
+        } else {
+            this.ingredientsId = ingredientsId.stream().map(k -> k.id).toArray(String[]::new);
+        }
+    }
+
+    public void setRandomValuesForIngredients() {
+        if (ingredientsId != null && ingredientsId.length > 0) {
+            for (int i = 0; i < ingredientsId.length; i++) {
+                ingredientsId[i] = RandomStringUtils.randomAlphabetic(30);
+            }
+        }
+    }
+}
